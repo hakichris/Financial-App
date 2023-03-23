c6c0dde546e047ba1baaf533e08aa481ed123d9c
Chris <hakizachrist1@gmail.com>
2023-03-23 11:43:45 +0200

    Update details.test

--- src/__test__/details.test.js
+++ src/__test__/details.test.js
@@ -1,6 +1,7 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { MemoryRouter,Route, Routes } from 'react-router';
 import CompanyFinancialDetails from '../pages/Details';
 import financialstatsReducer from '../redux/CompanyFinancialSlice';
 
@@ -17,7 +18,6 @@ const preloadedState = {
         calendarYear: 'alright',
         costOfRevenue: 3456,
         grossProfit: 3456,
-
       },
       {
         companySymbol: 'yeah',
@@ -45,11 +45,15 @@ describe('CompanyFinancialDetails component', () => {
   it('renders CompanyFinancialDetails correctly', () => {
     const { getByText } = render(
       <Provider store={store}>
-        <CompanyFinancialDetails />
-      </Provider>,
+        <MemoryRouter initialEntries={['/details/123']}>
+          <Routes>
+          <Route path="details/:id" element={<CompanyFinancialDetails />} />
+          </Routes>
+        </MemoryRouter>
+      </Provider>
     );
 
-    expect(getByText('alright')).toBeInTheDocument();
-    expect(getByText('alright')).toBeInTheDocument();
+    expect(screen.getByText(/okay/i)).toBeDefined();
+    expect(screen.getByText(/okay/i)).toBeDefined();
   });
 });
