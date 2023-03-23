3756286e5b121ca612be0b71031212ae8544ff1f
Chris <hakizachrist1@gmail.com>
2023-03-23 11:43:35 +0200

    Update companyfinancial.test

--- src/__test__/companyfinancial.test.js
+++ src/__test__/companyfinancial.test.js
@@ -1,8 +1,9 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import CompanyFinancial from '../pages/CompanyFinancial';
 import financialstatsReducer from '../redux/CompanyFinancialSlice';
+import { MemoryRouter } from 'react-router';
 
 const preloadedState = {
   finance: {
@@ -17,7 +18,7 @@ const preloadedState = {
         companySymbol: 'yeah',
         id: '123',
         reportedCurrency: 'okay',
-        calendarYear: 'alright',
+        calendarYear: 'cool',
       },
     ],
   },
@@ -34,11 +35,13 @@ describe('CompanyFinancial component', () => {
   it('renders CompanyFinancial correctly', () => {
     const { getByText } = render(
       <Provider store={store}>
+        <MemoryRouter>
         <CompanyFinancial />
+        </MemoryRouter>
       </Provider>,
     );
 
-    expect(getByText('alright')).toBeInTheDocument();
-    expect(getByText('alright')).toBeInTheDocument();
+    expect(screen.getByText(/alright/i)).toBeDefined();
+    expect(screen.getByText(/cool/i)).toBeDefined();
   });
 });
